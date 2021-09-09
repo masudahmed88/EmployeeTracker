392ac60729a057c15b3705becfa7c10c05db2080
Masud Ahmed <masudahmed88@gmail.com>
2021-09-08 23:20:35 -0400

    finished tables

---
+++ db/schema.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+  -- CREATE id, name COLUMNS
+  -- MAKE id AS PRIMARY KEY
+  -- TODO: YOUR CODE HERE
+     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+     name VARCHAR(30) NOT NULL 
+);
+
+CREATE TABLE role (
+  -- CREATE id AS INTERGER, title AS VARCHAR, salary AS DECIMAL, deplartment_id AS INTEGER
+  -- MAKE id As PRIMARY KEY
+  -- MAKE department_id AS FOREIGN KEY REFERENCING department TABLE AND
+  -- MAKE CONSTRAINT 'ON DELETE CASCADE' (WITHOUT QUOTES) ON THIS FOREIGN KEY
+  -- TODO: YOUR CODE HERE
+id INT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL NOT NULL,
+   department_id INT NOT NULL,
+   INDEX dep_ind (department_id),
+   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+  -- CREATE COLUMNS: id AS INT, first_name AS VARCHAR, last_name AS VARCHAR, role_id AS INTEGER, AND manager_id AS INT.
+  -- MAKE id As PRIMARY KEY
+  -- MAKE role_id AS FOREIGN KEY REFERENCING role TABLE AND MAKE CONSTRAINT ON DELETE CASCADE ON THIS FOREIGN KEY
+  -- MAKE manager_id AS FOREIGN KEY REFERENCING employee TABLE ITSELF AND MAKE CONSTRAINT ON DELETE SET NULL ON THIS FOREIGN KEY
+  -- TODO: YOUR CODE HERE
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+     first_name VARCHAR(30) NOT NULL, 
+     last_name VARCHAR(30) NOT NULL,
+     role_id INT NOT NULL, 
+     INDEX role_ind (role_id),
+     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+     manager_id INT, 
+     INDEX man_ind (manager_id),
+     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+);
